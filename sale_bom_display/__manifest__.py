f0f75c6f4723539a256de7d8f055eeb776603af2
Akash Kamble <akashkamble8308628955@gmail.com>
2026-04-09 17:37:43 +0530

    Added EC210 products data file

--- sale_bom_display/__manifest__.py
+++ sale_bom_display/__manifest__.py
@@ -3,6 +3,7 @@
     'version': '1.0',
     'depends': ['sale_management', 'mrp'],
     'data': [
+        'data/ec210_products.xml', 
         'views/sale_order_view.xml',
         'reports/sale_order_report.xml',
     ],
