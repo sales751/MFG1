5eddb52b9246da15bc7cc3465ecaccaf4b2e60bd
Akash Kamble <akashkamble8308628955@gmail.com>
2026-04-10 12:31:05 +0530

    Added EC210 BOMs for 8 hoses and 1 kit

--- sale_bom_display/__manifest__.py
+++ sale_bom_display/__manifest__.py
@@ -4,6 +4,7 @@
     'depends': ['sale_management', 'mrp'],
     'data': [
         'data/ec210_products.xml', 
+        'data/ec210_boms.xml',
         'views/sale_order_view.xml',
         'reports/sale_order_report.xml',
     ],
